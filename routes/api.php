c1f319566948c7076c27d9334613e208a355538e
marislutsar <maris.lutsar@tptlive.ee>
2025-03-01 09:10:47 +0200

    Add auth api routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\PublicController;
 use Illuminate\Http\Request;
@@ -11,4 +13,8 @@ Route::get('/user', function (Request $request) {
 
 Route::get('/posts', [PublicController::class, 'index']);
 Route::get('/posts/{post}', [PublicController::class, 'post']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+
+
 
